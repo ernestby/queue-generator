8f3d5155cfa87301ac501cd13a651744b63926dd
Ernest Stanbekov <ernestby@gmail.com>
2021-10-03 23:20:08 +0600

    create new layout

---
+++ queue-client/src/components/layouts/AuthLayout.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="auth-layout">
+    <div class="auth-layout__content"><slot /></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AuthLayout",
+};
+</script>
+
+<style lang="scss" scoped>
+.auth-layout {
+  &__content {
+    height: 100vh;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+}
+</style>
---
+++ queue-client/src/components/layouts/ListLayout.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="list-layout">
+    <Header />
+    <slot />
+  </div>
+</template>
+
+<script>
+import Header from "@/components/common/Header";
+export default {
+  name: "ListLayout",
+  components: {
+    Header,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.list-layout {
+  &__username {
+    margin-right: 15px;
+  }
+}
+</style>
